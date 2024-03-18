72b4438a145aa05265c87de8dbb8e10870006f6f
Nabil-Arrouss <99538380+Nabil-Arrouss@users.noreply.github.com>
2024-03-18 22:12:34 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Create new account
+        Account nabilAccount = new Account();
+
+        nabilAccount.setNumber("12345");
+        nabilAccount.setBalance(10000d);
+        nabilAccount.setCustomerName("Nabil Arrouss");
+        nabilAccount.setCustomerEmail("Nabilarrouss@gmail.com");
+        nabilAccount.setCustomerPhone("06 00 000 0000");
+
+
+        // Print information
+        System.out.println(nabilAccount.getNumber());
+        System.out.println(nabilAccount.getBalance());
+
+        // Testing
+        nabilAccount.withdrawnFunds(100d);
+        nabilAccount.depositFunds(250);
+        nabilAccount.withdrawnFunds(50);
+        nabilAccount.withdrawnFunds(200);
+        nabilAccount.depositFunds(100);
+        nabilAccount.withdrawnFunds(45.55);
+        nabilAccount.withdrawnFunds(54.46);
+    }
+}
